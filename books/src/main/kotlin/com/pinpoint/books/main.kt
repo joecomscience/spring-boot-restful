9f79fea1f4b3935935b34cf1def47223a12e3d8c
joewalker <assassin.joe@hotmail.com>
2019-08-26 10:01:01 +0700

    Fix deprecate properties in gradle and add main class

--- books/src/main/kotlin/com/pinpoint/books/main.kt
+++ books/src/main/kotlin/com/pinpoint/books/main.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application
+class Main
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    SpringApplication.run(Main::class.java, *args)
 }
